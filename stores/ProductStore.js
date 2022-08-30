c7f86d20fb1c720638cde9f224f4aa045854e53c
devangg37 <devangg37@gmail.com>
2022-08-31 01:39:32 +0900

    Added contentful

--- stores/ProductStore.js
+++ stores/ProductStore.js
@@ -34,8 +34,11 @@ export const useProductStore = defineStore("ProductStore", {
   },
   actions: {
     async fetchProducts() {
-      const res = await $fetch("/api/products");
-      this.products = res;
+      const {$contentful} = useNuxtApp();
+      const res = await $contentful.getEntries({
+        content_type: "product",
+      });
+      this.products = res.items;
       return this.products;
     },
     async fetchProduct(id) {
